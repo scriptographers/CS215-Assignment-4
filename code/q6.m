67cf490261e5158ab9c97f7902804d5746992315
Harshit Varma <varmaharshit2@gmail.com>
2020-11-07 10:06:06 +0530

    Q6: Moved function inside the main file

--- code/q6.m
+++ code/q6.m
@@ -79,3 +79,10 @@ ylabel("Eigenvalue");
 title("Question 6: First 10 Eigenvalues");
 saveas(gca, "plots/q6ii.jpg");
 hold off;
+
+% MinMax scaler for scaling images to [0, 1]
+function [y] = toImg(x, shape)
+    y = x - min(x);
+    y = y/max(y);
+    y = reshape(y, shape);
+end
