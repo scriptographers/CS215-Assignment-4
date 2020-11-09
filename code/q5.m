2882721a026f63799da05f49e17b9c376ee5b198
devansh-dvj <devansh.dvj@gmail.com>
2020-11-10 02:15:07 +0530

    Q5: Completed code

--- code/q5.m
+++ code/q5.m
@@ -2,8 +2,6 @@ clc;
 clear;
 close all;
 
-tic;
-
 DATA_PATH = "../data/mnist.mat";
 load(DATA_PATH, "-mat"); % Load data
 N = length(digits_train);
@@ -17,11 +15,36 @@ for digit=0:9
     digit_data = train_data(:, labels_train == digit);
     mean = sum(digit_data, 2)/count; % MLE of mean
     cov = (digit_data-mean)*(digit_data'-mean')/count; % MLE of cov
-    [Q, D] = eig(cov);
-    dia = sort(diag(D),'descend');
-    dia(dia<=0)=0; % Fixing numerical error in eig() due to precision
+    
+    % Eigendecomposition of cov: eigs() is much much faster than eig()
+    [Q, L] = eigs(cov, 84); % Get 84 largest eigenvalues and corresponding eigenvectors
+    % Q represents 84 columns representing a 84-dimensional basis
+    
+    test_data = digit_data(:,end); % Seemed to give nice results
+    reduced_data = reduce_dim(test_data, Q, mean);
+    
+    subplot(1,2,1);
+    imagesc(reshape(test_data, [WIDTH WIDTH]));
+    title("Original Image");
+    pbaspect([1 1 1]);
+    axis off;
+    subplot(1,2,2);
+    imagesc(reshape(reconstruct_img(reduced_data, Q, mean), [WIDTH WIDTH]));
+    title("Reconstructed Image");
+    pbaspect([1 1 1]);
+    axis off;
+    
+    sgtitle(sprintf("Digit %i", digit));
+    colormap('gray');
+    saveas(gcf, sprintf("plots/q5/comp_%i.jpg", digit)); % Save current figure
 end
 
 close all;
 
-toc;
+function [reduced_data] = reduce_dim(digit_img, Q, mean)
+    reduced_data = Q'*(digit_img-mean);
+end
+
+function [reduced_img] = reconstruct_img(reduced_data, Q, mean)
+    reduced_img = mean + Q*reduced_data;
+end
